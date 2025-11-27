1fe029e25bb0b771111cce7247fde495aaff8b95
“pank886” <1134926811@qq.com>
2025-11-27 19:45:36 +0800

    v0.4.1断言读取（包含断言）

--- common/debugtilk.py
+++ common/debugtilk.py
@@ -10,10 +10,29 @@ class DebugTalk:
         self.read = ReadYamlData()
 
     def get_extract_order_data(self, data, randoms):
-        if randoms not in [0, 1]:
+        if randoms not in [0, -1, -2]:
             return data[randoms - 1]
 
-    def get_extract_data(self, node_name, randoms = None):
+    def get_extract_data(self, node_name, sec_node_name = None, randoms = None):
+        """
+        读取extract.yaml的数据
+        :param node_name: 读取extract.yaml文件中的key值
+        :param sec_node_name: 读取嵌套的key值
+        :param random: None 数组顺序展示 0 随机读取 -1 字符串展示
+        :return:
+        """
+        data = self.read.get_extract_yaml(node_name, sec_node_name)
+        if randoms is not None:
+            randoms = int(randoms)
+            data_value = {
+                randoms : self.get_extract_order_data(data, randoms),
+                0 : random.choice(data),
+                -1 : ','.join(data)
+            }
+            data = data_value[randoms]
+        return data
+
+    def get_extract_data_list(self, node_name, randoms = None):
         """
         读取extract.yaml的数据
         :param node_name: 读取extract.yaml文件中的key值
@@ -30,6 +49,7 @@ class DebugTalk:
             }
             data = data_value[randoms]
         return data
+
     def md5_params(self, params):
         return 'sadfsdf' + str(params)
 
